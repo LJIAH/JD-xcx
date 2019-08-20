3e8c7946c2bca92b22000052bbecd858521297dc
LJIAH <githublin@foxmail.com>
2019-08-20 22:04:29 +0800

    学习到产品分类

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,53 +1,36 @@
-const interfaces = require("../../utils/urlconfig.js")
-// pages/category/index.js
+const interfaces = require('../../utils/urlconfig.js')
+// pages/list/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    navLeftItems: [],
-    navRightItems: [],
-    curIndex: 0
+    prolist: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const self = this;
+    // console.log(options)
+    wx.setNavigationBarTitle({
+      title: options.title,
+    })
     wx.showLoading({
       title: '加载中...',
     })
+    const self = this;
     wx.request({
-      url: interfaces.productions,
-      header: {
-        "content-type": "application/json"
-      },
+      url: interfaces.productionsList,
       success(res){
         // console.log(res.data)
         self.setData({
-          navLeftItems: res.data.navLeftItems,
-          navRightItems: res.data.navRightItems
+          prolist: res.data
         })
-        wx.hideLoading();
+
+        wx.hideLoading()
       }
-      
-    })
-  },
-  switchRightTab(e){
-    // console.log(e)
-    let index = parseInt(e.currentTarget.dataset.index);
-    this.setData({
-      curIndex: index
-    })
-  },
-  showListView(e){
-    // console.log(e)
-    let txt = e.currentTarget.dataset.txt;
-    // 导航跳转方法
-    wx.navigateTo({
-      url: '/pages/list/index?title=' + txt,
     })
   },
 
--- utils/urlconfig.js
+++ utils/urlconfig.js
@@ -1,6 +1,8 @@
 const domain = "https://sleepy-gorge-75413.herokuapp.com";
 
 const interfaces = {
-  homepage: domain + "/api/profiles/homepage"
+  homepage: domain + "/api/profiles/homepage",
+  productions: domain + "/api/profiles/productions",
+  productionsList: domain + "/api/profiles/productionsList"
 }
 module.exports = interfaces
